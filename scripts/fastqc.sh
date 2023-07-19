d871cebd2a5d4c71f309dfee5bbf7d96be171620
Ubuntu <ubuntu@ip-10-10-255-206.us-east-2.compute.internal>
2023-07-19 20:49:08 +0000

    fastqc not commitef?

--- scripts/fastqc.sh
+++ scripts/fastqc.sh
@@ -12,3 +12,17 @@ docker run -v /root/vaxart-batch-output/outputApril192023JS/pass:/input \
 -v /vaxart-batch-output/outputApril192023JS_fastqc/:/output 620901718958.dkr.ecr.us-east-2.amazonaws.com/fastqc \
 fastqc $file -o /output -dir /output 
 done
+
+
+
+
+Minimap:
+  
+  
+  mkdir -p ${id}/minimap
+  cat ${folder}/pass/*.fastq > ${id}/minimap/${id}.fastq
+  minimap2 --MD -a ${reference_genome}/${genome}.fa ${id}/minimap/${id}.fastq -o ${id}/minimap/${id}.bam
+
+docker run -v /root/vaxart-batch-output/outputApril192023JS/pass:/input \
+-v /vaxart-batch-output/outputApril192023JS/minimap:/output 620901718958.dkr.ecr.us-east-2.amazonaws.com/nanopanel2 \
+minimap2 --help
